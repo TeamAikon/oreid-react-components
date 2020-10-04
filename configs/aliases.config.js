a294db213bad4af8ef166a1ad8a87f42c866dfc0
Tray Lewin <tray.lewin@live.com>
2020-10-03 22:29:24 -0700

    rename src directory to packages

--- configs/aliases.config.js
+++ configs/aliases.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 // __dirname gives exact path where the file is, in this case: PROJECT/configs. It is needed to add ../ at the beginning of the aliases to solve correct path.
 
 const aliases = {
-    'oreid-login-button': path.resolve(__dirname, '../src/LoginButton/src/index'),
+    'oreid-login-button': path.resolve(__dirname, '../packages/LoginButton/src/index'),
 };
 
 module.exports = { aliases }
