6d749faa5bbf8abb38ef0c2108f769473025e584
artmanque <i@xn--rtman-ewa.com>
2021-07-16 22:11:32 +0500

    Use singal quotes

--- packages/OreIdWebWidget/src/OreIdWebWidget.tsx
+++ packages/OreIdWebWidget/src/OreIdWebWidget.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { WebWidget } from "oreid-js";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { WebWidget } from 'oreid-js';
 
 declare const window: any;
 let OreIdWebWidget: any;
 
 if (window) {
   const widget = WebWidget.createWebWidget();
-  window["WrapperComponent"] = widget;
-  OreIdWebWidget = window.WrapperComponent.driver("react", {
+  window['WrapperComponent'] = widget;
+  OreIdWebWidget = window.WrapperComponent.driver('react', {
     React,
     ReactDOM,
   });
