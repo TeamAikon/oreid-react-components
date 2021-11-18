f8b5999d897131381b433b4d87dce9d1ad6b0dc9
Bruno Motta <wadge.motta@gmail.com>
2021-11-18 00:42:37 -0300

    remove unecessary code

--- packages/OreIdWalletConnect/rollup.config.js
+++ packages/OreIdWalletConnect/rollup.config.js
@@ -1,17 +1,17 @@
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "rollup-plugin-typescript2";
-import sass from "rollup-plugin-sass";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from 'rollup-plugin-typescript2'
+import sass from 'rollup-plugin-sass'
 // import copy from "rollup-plugin-copy";
 
 export default {
-  input: ["src/index.ts", "src/OreIdWalletConnect/index.ts"],
+  input: ['src/index.ts'],
   output: [
     {
-      dir: "build",
-      format: "cjs",
-      sourcemap: true
-    }
+      dir: 'build',
+      format: 'cjs',
+      sourcemap: true,
+    },
   ],
   preserveModules: true, // Important if we want to code split
   plugins: [
@@ -19,21 +19,7 @@ export default {
     commonjs(),
     typescript({ useTsconfigDeclarationDir: true }),
     sass({
-      insert: true
+      insert: true,
     }),
-    // copy({
-    //   targets: [
-    //     {
-    //       src: "src/variables.scss",
-    //       dest: "build",
-    //       rename: "variables.scss"
-    //     },
-    //     {
-    //       src: "src/typography.scss",
-    //       dest: "build",
-    //       rename: "typography.scss"
-    //     }
-    //   ]
-    // })
-  ]
-};
+  ],
+}
