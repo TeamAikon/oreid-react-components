9f467db2efa2db18be0857ef951ad222a5c6e71b
Tray Lewin <tray.lewin@live.com>
2021-11-19 15:48:39 -0800

    use cleaner spinner

--- packages/OreIdWalletConnect/src/Loading/Loading.tsx
+++ packages/OreIdWalletConnect/src/Loading/Loading.tsx
@@ -1,12 +1,35 @@
 import React from 'react'
-import { LogoIcon } from './LogoIcon'
-
 import './Loading.scss'
 
-export const Loading: React.FC = () => {
+interface LoadingProps {
+  backgroundColor?: string
+  brandColor?: string
+}
+
+export const Loading: React.FC<LoadingProps> = ({ backgroundColor, brandColor }) => {
+  const containerStyle: React.CSSProperties = {
+    // backgroundColor: backgroundColor || defaultBackgroundColor,
+    display: 'flex',
+    minHeight: '40vh',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+  }
   return (
-    <div className="oreIdWalletConnect-loading">
-      <LogoIcon />
+    <div style={containerStyle}>
+      <div>
+        <div
+          style={{
+            height: '40px',
+            width: '40px',
+            boxSizing: 'border-box',
+            border: ' 3px solid rgba(0, 0, 0, 0.2)',
+            borderTopColor: brandColor || 'rgba(235, 239, 242, 0.8)',
+            borderRadius: '100%',
+            animation: 'rotation 0.7s infinite linear',
+          }}
+        />
+      </div>
     </div>
   )
 }
