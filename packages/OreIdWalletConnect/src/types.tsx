d24982eb8dff9eed8364690b1a0b3e63136818d5
Abdul R. Tayyab <i@xn--rtman-ewa.com>
2021-11-23 16:13:33 +0500

    type: WalletConnectSession | remove redundant optional props

--- packages/OreIdWalletConnect/src/types.tsx
+++ packages/OreIdWalletConnect/src/types.tsx
@@ -1,10 +1,7 @@
 import WalletConnectClient from '@walletconnect/client'
 import { JsonRpc } from '@walletconnect/types'
-// import { GQLModel } from '@aikon/oreid-shared/dist/models'
-// interface copied from shared @aikon/oreid-shared/dist/models
+
 interface WalletConnectSession {
-  __typename?: 'WalletConnectSession'
-  _id?: string
   key: string
   peerId: string
   peerUrl: string
