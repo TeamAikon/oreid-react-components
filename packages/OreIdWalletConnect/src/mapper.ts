621760cb086fa57154143afd6f3d907de2105343
Tray Lewin <tray.lewin@live.com>
2021-12-02 11:00:02 -0800

    use unified chainNetworkMap metadata

--- packages/OreIdWalletConnect/src/mapper.ts
+++ packages/OreIdWalletConnect/src/mapper.ts
@@ -1,4 +1,5 @@
 import WalletConnectClient from '@walletconnect/client'
+import { chainNetworkMap } from '.'
 import { ChainNetwork, Connection, WalletConnectRef, OreIDWalletConnectConfig } from './types'
 
 export const mapWalletConnectRefToConnection = (connection: WalletConnectRef): Connection => {
@@ -44,35 +45,20 @@ export const mapConnectionToWalletConnectRefSession = (connection: Connection, c
   return session
 }
 
-const mapChainIdToChainNetwork = (chainId: number): ChainNetwork => {
-  if (chainId === 1) return ChainNetwork.EthMain
-  if (chainId === 3) return ChainNetwork.EthRopsten
-  if (chainId === 4) return ChainNetwork.EthRinkeby
-  if (chainId === 40) return ChainNetwork.TelosMain
-  if (chainId === 41) return ChainNetwork.TelosTest
-  if (chainId === 59) return ChainNetwork.EosMain
-  if (chainId === 95) return ChainNetwork.EosKylin
-  throw Error(`Unsupported chainId: ${chainId}`)
+export const mapChainIdToChainNetwork = (chainId: number): ChainNetwork => {
+  const chainNetwork = chainNetworkMap.find((cnm) => cnm.id === chainId)?.chainNetwork
+  if (!chainNetwork) throw Error(`Chain Id ${chainId} not supported`)
+  return chainNetwork
 }
 
 export const mapChainNetworkToChainId = (chainNetwork: string): number => {
-  if (chainNetwork === ChainNetwork.EthMain.toString()) return 1
-  if (chainNetwork === ChainNetwork.EthRopsten.toString()) return 3
-  if (chainNetwork === ChainNetwork.EthRinkeby.toString()) return 4
-  if (chainNetwork === ChainNetwork.TelosMain.toString()) return 40
-  if (chainNetwork === ChainNetwork.TelosTest.toString()) return 41
-  if (chainNetwork === ChainNetwork.EosMain.toString()) return 59
-  if (chainNetwork === ChainNetwork.EosKylin.toString()) return 95
-  throw Error(`Unsupported chainNetwork: ${chainNetwork}`)
+  const chainId = chainNetworkMap.find((cnm) => cnm.chainNetwork === chainNetwork)?.id
+  if (!chainId) throw Error(`Chain network ${chainNetwork} not supported`)
+  return chainId
 }
 
 export const mapChainNetworkToName = (chainNetwork: string): string => {
-  if (chainNetwork === ChainNetwork.EthMain.toString()) return 'Ethereum'
-  if (chainNetwork === ChainNetwork.EthRopsten.toString()) return 'Ethereum Ropsten'
-  if (chainNetwork === ChainNetwork.EthRinkeby.toString()) return 'Ethereum Rinkeby'
-  if (chainNetwork === ChainNetwork.TelosMain.toString()) return 'Telos Mainnet'
-  if (chainNetwork === ChainNetwork.TelosTest.toString()) return 'Telos Testnet'
-  if (chainNetwork === ChainNetwork.EosMain.toString()) return 'EOS Mainnet'
-  if (chainNetwork === ChainNetwork.EosKylin.toString()) return 'EOS Kylin'
-  throw Error(`Unsupported chainNetwork: ${chainNetwork}`)
+  const chainName = chainNetworkMap.find((cnm) => cnm.chainNetwork === chainNetwork)?.name
+  if (!chainName) throw Error(`Chain network ${chainNetwork} not supported`)
+  return chainName
 }
