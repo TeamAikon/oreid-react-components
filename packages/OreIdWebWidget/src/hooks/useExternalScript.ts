cbc2ef7c238cd6f6bb24ec849cf3af2bb2b4ae3b
Tray Lewin <tray.lewin@live.com>
2022-02-06 13:56:49 -0800

    add useExternalScript hook

---
+++ packages/OreIdWebWidget/src/hooks/useExternalScript.ts
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+
+export enum ScriptStatus {
+  Idle = 'idle',
+  Loading = 'loading',
+  Ready = 'ready',
+  ErrorDownloading = 'errorDownloading',
+  ErrorExecution = 'errorExecution'
+}
+
+/** Downloads a 'application/javascript' script from a url and attaches to window object
+ *  Returns 'ready' or 'errorDownloading'
+ */
+export const useExternalScript = (url: string) => {
+  let [status, setStatus] = useState(url ? ScriptStatus.Loading : ScriptStatus.Idle);
+
+  useEffect(() => {
+    if (!url) {
+      setStatus(ScriptStatus.Idle)
+      return
+    }
+    let script: HTMLScriptElement | null = null
+
+    const handleScriptEvent = (e: Event) => {
+      setStatus(e.type === "load" ? ScriptStatus.Ready : ScriptStatus.ErrorDownloading);
+    }
+
+    if (!script) {
+      script = document.createElement("script")
+      script.type = "application/javascript"
+      script.src = url
+      script.async = true
+      script.addEventListener("load", handleScriptEvent)
+      script.addEventListener("error", handleScriptEvent)
+      document.body.appendChild(script)
+    }
+
+    return () => {
+      script?.removeEventListener("load", handleScriptEvent)
+      script?.removeEventListener("error", handleScriptEvent)
+    };
+  }, [url])
+
+  return status
+}
