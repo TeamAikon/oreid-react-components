5f6ff764e760d835bb66e7fef24fe1d342892094
Bruno Motta <wadge.motta@gmail.com>
2022-03-30 21:48:35 -0300

    Adding Observer to provider

--- packages/OreIdReact/src/components/OreidProvider/OreIdProvider.tsx
+++ packages/OreIdReact/src/components/OreidProvider/OreIdProvider.tsx
@@ -3,6 +3,7 @@ import { OreId, UserData } from "oreid-js";
 import { OreIdWebWidget } from "oreid-webwidget";
 import { OreIdContext } from "src/OreIdContext";
 import { useState } from "react";
+import { Observables } from "../Observables";
 
 interface Props {
 	oreId: OreId;
@@ -14,9 +15,23 @@ export const OreidProvider: React.FC<Props> = ({
 	webWidget,
 }) => {
 	const [user, setUser] = useState<UserData | undefined>(undefined);
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const [accessToken, setAccessToken] = useState<string | undefined>(undefined);
 
 	return (
-		<OreIdContext.Provider value={{ oreId, webWidget, user, setUser }}>
+		<OreIdContext.Provider
+			value={{
+				oreId,
+				webWidget,
+				user,
+				setUser,
+				isLoggedIn,
+				setIsLoggedIn,
+				accessToken,
+				setAccessToken,
+			}}
+		>
+			<Observables />
 			{children}
 		</OreIdContext.Provider>
 	);
