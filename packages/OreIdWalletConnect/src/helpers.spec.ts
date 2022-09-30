b4f894ec00321583b5c2550b107c6a4d1e3f65d2
Warrick FitzGerald <40346010+WarrickFitz@users.noreply.github.com>
2022-09-30 12:18:30 -0400

    Add AVAX and TLOS walletconnect

--- packages/OreIdWalletConnect/src/helpers.spec.ts
+++ packages/OreIdWalletConnect/src/helpers.spec.ts
@@ -6,6 +6,10 @@ it('Check if has support', () => {
   expect(hasChainSupport(ChainNetwork.EthRinkeby)).toBe(true)
   expect(hasChainSupport(ChainNetwork.EthRopsten)).toBe(true)
   expect(hasChainSupport(ChainNetwork.EthGoerli)).toBe(true)
+  expect(hasChainSupport(ChainNetwork.TelosEvmMain)).toBe(true)
+  expect(hasChainSupport(ChainNetwork.TelosEvmTest)).toBe(true)
+  expect(hasChainSupport(ChainNetwork.AvalancheC_Main)).toBe(true)
+  expect(hasChainSupport(ChainNetwork.AvalancheC_Fuji)).toBe(true)
 })
 
 it('Check if has no support', () => {
